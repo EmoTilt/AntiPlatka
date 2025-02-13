37821254e93a141f4d3d544b3d9f015a05879c24
2007 <crocuscity2007@gmail.com>
2025-02-13 21:18:15 +0300

    fix romki huesosa

--- src/commands/other/Huify.ts
+++ src/commands/other/Huify.ts
@@ -1,22 +1,18 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import Command from '../../structures/Command';
 import BotClient from '../../structures/Client';
+import { Command } from '../../interfaces';
 
 export default class HuifyCommand implements Command {
     slash = new SlashCommandBuilder()
         .setName('huify')
         .setDescription('Имя - хуимя')
         .addStringOption(option =>
-            option.setName('Имя')
-                .setDescription('Имя чтобы сделать хуимя')
-                .setRequired(true));
+            option.setName('Имя').setDescription('Имя чтобы сделать хуимя').setRequired(true),
+        ) as SlashCommandBuilder;
 
     execute(client: BotClient, interaction: ChatInputCommandInteraction): void {
         const name = interaction.options.getString('Имя');
 
-        interaction
-            .reply(name + ` - ху` + name.slice(1))
-            .catch(error => client.logger.error(error));
+        interaction.reply(name + ` - ху` + name?.slice(1)).catch(error => client.logger.error(error));
     }
 }
-
