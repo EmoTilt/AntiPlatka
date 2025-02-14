187d4f2db3bca60087279e36ac766b72dc69011d
2007 <crocuscity2007@gmail.com>
2025-02-14 20:22:57 +0300

    переписал говнокод ромки

--- src/commands/other/Huify.ts
+++ src/commands/other/Huify.ts
@@ -2,27 +2,6 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import BotClient from '../../structures/Client';
 import { Command } from '../../interfaces';
 
-function huify(name) {
-    var i = 0;
-
-    while ('бвгджзйклмнпрстфхцчшщ'.includes(name[i]) && (i < name.length)) i += 1;
-
-    if (i >= name.length - 1) {
-        return 'ху' + name.slice(1);
-    }
-    else {
-        name = name.slice(i);
-
-        if ('аоуыэ'.includes(name[0])) {
-
-            const letters = new Map([['а', 'я'], ['о', 'ё'], ['ы', 'и'], ['э', 'e'], ['у', 'ю']]);
-            return 'ху' + letters.get(name[0]) + name.slice(1);
-
-        }
-        else return 'ху' + name;
-    }
-}
-
 export default class HuifyCommand implements Command {
     slash = new SlashCommandBuilder()
         .setName('huify')
@@ -33,7 +12,29 @@ export default class HuifyCommand implements Command {
 
     execute(client: BotClient, interaction: ChatInputCommandInteraction): void {
         const name = interaction.options.getString('name');
+        if (!name) return;
 
-        interaction.reply(name + ` - ` + huify(name)).catch(error => client.logger.error(error));
+        interaction.reply(name + ` - ` + this.huify(name)).catch(error => client.logger.error(error));
+    }
+
+    huify(name: string) {
+        let i = 0;
+        while ('бвгджзйклмнпрстфхцчшщ'.includes(name[i]) && i < name.length) i += 1;
+
+        if (i >= name.length - 1) {
+            return 'ху' + name.slice(1);
+        } else {
+            name = name.slice(i);
+            if ('аоуыэ'.includes(name[0])) {
+                const letters = new Map([
+                    ['а', 'я'],
+                    ['о', 'ё'],
+                    ['ы', 'и'],
+                    ['э', 'e'],
+                    ['у', 'ю'],
+                ]);
+                return 'ху' + letters.get(name[0]) + name.slice(1);
+            } else return 'ху' + name;
+        }
     }
 }
