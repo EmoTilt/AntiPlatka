5700229288b06cd24b2c0a3484e7d622bac8c105
Нилтон <99443380+n1lton@users.noreply.github.com>
2025-02-14 18:30:43 +0300

    Update Huify.ts

--- src/commands/other/Huify.ts
+++ src/commands/other/Huify.ts
@@ -2,6 +2,28 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import BotClient from '../../structures/Client';
 import { Command } from '../../interfaces';
 
+function huify(name) {
+    var i = 0;
+
+    while ('бвгджзйклмнпрстфхцчшщ'.includes(name[i]) && (i < name.length)) i += 1;
+
+    console.log(i);
+    if (i >= name.length - 1) {
+        return 'ху' + name.slice(1);
+    }
+    else {
+        name = name.slice(i);
+
+        if ('аоуыэ'.includes(name[0])) {
+
+            const letters = new Map([['а', 'я'], ['о', 'ё'], ['ы', 'и'], ['э', 'e'], ['у', 'ю']]);
+            return 'ху' + letters.get(name[0]) + name.slice(1);
+
+        }
+        else return 'ху' + name;
+    }
+}
+
 export default class HuifyCommand implements Command {
     slash = new SlashCommandBuilder()
         .setName('huify')
@@ -13,6 +35,6 @@ export default class HuifyCommand implements Command {
     execute(client: BotClient, interaction: ChatInputCommandInteraction): void {
         const name = interaction.options.getString('name');
 
-        interaction.reply(name + ` - ху` + name?.slice(1)).catch(error => client.logger.error(error));
+        interaction.reply(name + ` - ` + huify(name)).catch(error => client.logger.error(error));
     }
 }
