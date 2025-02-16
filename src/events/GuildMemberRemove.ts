fed7cb2b0e1639021ba10cec26cc236c3d777c4b
2007 <crocuscity2007@gmail.com>
2025-02-16 13:51:24 +0300

    fix guildmemberremove

--- src/events/GuildMemberRemove.ts
+++ src/events/GuildMemberRemove.ts
@@ -8,9 +8,31 @@ export default class GuildMemberRemove implements Event {
 
     execute(client: BotClient, member: GuildMember): void {
         member.ban({ reason: 'Покинул сервер' }).catch(error => client.logger.error(error));
-        const channel = client.channels.cache.get(client.config.bannedChannel) as TextChannel;
+        client.db.userPoll
+            .findFirst({
+                where: { memberId: member.id },
+            })
+            .then(memberDb => {
+                if (memberDb) {
+                    client.db.userPoll
+                        .delete({
+                            where: { memberId: member.id },
+                        })
+                        .catch(error => client.logger.error(error));
+                    const voteChannel = client.channels.cache.get(client.config.voteChannel);
+                    if (voteChannel && voteChannel instanceof TextChannel) {
+                        voteChannel.messages
+                            .fetch(memberDb.messageId)
+                            .then(message => message.delete())
+                            .catch(error => client.logger.error(error));
+                    }
+                }
+            })
+            .catch(error => client.logger.error(error));
 
-        channel
+        const bannedChannel = client.channels.cache.get(client.config.bannedChannel) as TextChannel;
+
+        bannedChannel
             .send(`${member.displayName} (${member.user.tag}) забанен. Причина: **покинул сервер**.`)
             .catch(error => client.logger.error(error));
     }
